bc81f3d81c9a2bbca40d341682f44bce301de6bd
hongchiong <hongchiong@gmail.com>
2019-05-28 02:26:27 +0800

    render cards from data. react bootstrap styling. query listings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,104 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { logoMap } from './logos';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 function App() {
+  const [listings, setListings] = useState({ data: []});
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const result = await axios('https://api.chanced.co/v1/jobs');
+      //change the links for logos
+      for (let i = 0; i < result.data.data.length; i++) {
+        result.data.data[i].company.logo = logoMap[result.data.data[i].company.logo];
+        console.log(result.data.data[i].company.logo);
+      }
+      setListings(result.data);
+    }
+
+    fetchListings();
+  }, []);
+
+  let timer;
+  const handleChange= (e) => {
+    const fetchQueryListings = async () => {
+      const result = await axios(`https://api.chanced.co/v1/jobs?query=${query}`);
+      //change the links for logos
+      for (let i = 0; i < result.data.data.length; i++) {
+        result.data.data[i].company.logo = logoMap[result.data.data[i].company.logo];
+        console.log(result.data.data[i].company.logo);
+      }
+      setListings(result.data);
+    };
+
+    const query = e.target.value;
+    if (timer !== undefined) { clearTimeout(timer) }
+    timer = setTimeout(fetchQueryListings, 300);
+  };
+
+  
   return (
     <div>
+      <InputGroup className="my-3">
+        <InputGroup.Prepend>
+          <InputGroup.Text >Search Job</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          placeholder="Search by job title ..."
+          onChange={handleChange}
+        />
+      </InputGroup>
+      {
+        listings.data.map(listing => (
+          <Card key={listing.ID}>
+            <Container>
+              <Row>
+
+                <Col md={2} className="d-flex align-items-center">
+                  <Image className="logo" alt="company-logo" src={listing.company.logo} height="128" width="128" roundedCircle/>
+                </Col>
+
+                <Col md={6} className="d-flex align-items-center">
+                  <Card.Body>
+                    <div className="my-3 title">
+                      {listing.title}
+                    </div>
+                    <div className="my-3">
+                      <span className="companyname">{listing.company.name}</span> | <span className="country">{listing.country}</span>
+                    </div>
+                  </Card.Body>
+                </Col>
+
+                <Col md={4} className="d-flex align-items-center">
+                  <Card.Body >
+                    <div className="my-3 text-right">
+                      <span><i className="fas fa-bookmark mr-2 ml-4"></i> Save </span> 
+                      <span><i className="fas fa-share-alt mr-2 ml-4"></i> Share </span>
+                    </div>
+                    <div className="my-3 text-right"> 
+                        {
+                          (Date.now() - (new Date(listing.updated_at)).getTime()) > 60
+                          ? <span>{Math.floor((Date.now() - (new Date(listing.updated_at)).getTime())/60000/60)} hours ago</span>
+                          : <span>{(Date.now() - (new Date(listing.updated_at)).getTime())/60000} mins ago</span>
+                        }
+                    </div>
+                  </Card.Body>
+                </Col>
+
+              </Row>
+            </Container>
+          </Card>
+        ))
+      }
     </div>
   );
 }
---
+++ src/logos.js
@@ -0,0 +1,9 @@
+export const logoMap = {
+  "/companies/logos/google.svg": "https://media.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_400_400/0?e=1567036800&v=beta&t=eVA9ftTDaL6XD4QzhCesaLkyQ-AgRsyDjcjPnRmoX4E",
+  "/companies/logos/bynd_artisan.svg": "https://media.licdn.com/dms/image/C510BAQG8B7qTGn-QMw/company-logo_400_400/0?e=1567036800&v=beta&t=SnqAHd5Db2uGHe4Tp2-cdVGn6kffN-ziKi_jG8udeGU",
+  "/companies/logos/4x_labs.svg": "https://media.licdn.com/dms/image/C510BAQEQ1M6COPt-BA/company-logo_400_400/0?e=1567036800&v=beta&t=7rofMJ_DMyodRa8_tZgbH3PE1-Ce1afia1ZHu4n_uWk",
+  "/companies/logos/whitecoat_global.svg": "https://media.licdn.com/dms/image/C510BAQFldChm61k7LA/company-logo_400_400/0?e=1567036800&v=beta&t=SxoFskPrcQNRLuuIhc_dAJ2MCiEcVsRGfzFfzZnLF5o",
+  "/companies/logos/nimbly_technologies.svg": "https://media.licdn.com/dms/image/C510BAQHzkugIvUG0aw/company-logo_400_400/0?e=1567036800&v=beta&t=XD-1xCFev7YCOBJWlALP9EBBlDaw7a9v68V4Hb-UDvc",
+  "/companies/logos/zalora_group.svg": "https://media.licdn.com/dms/image/C4D0BAQGeZOSwuB9rGg/company-logo_400_400/0?e=1567036800&v=beta&t=-E_d16x9vdd5GkCdqyfXa1q1jFOaUN5X7q_CNNQHYUM",
+  "/companies/logos/razer.svg": "https://media.licdn.com/dms/image/C510BAQGbbLSn1OuT_A/company-logo_400_400/0?e=1567036800&v=beta&t=_hJMUg6mSo0-KLpL8XhM1uHn-lyHDVaz977CXGyciYM"
+}
